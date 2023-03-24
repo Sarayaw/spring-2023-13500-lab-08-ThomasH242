9e3e8ac48b64d9eacf5ec768fd486c0fbad5bb43
ThomasH242 <Thomas.huang45@myhunter.cuny.edu>
2023-03-23 22:22:13 -0400

    done

--- funcs.cpp
+++ funcs.cpp
@@ -19,20 +19,20 @@ void invert(std::string input){
   writeImage("taskA.pgm",image,h,w);
 }
 void inverthalf(std::string input){
-    int img[MAX_H][MAX_W];
-    int h, w;
-    readImage(input, img, h, w);
-    int imga[MAX_H][MAX_W];
-    for(int i = 0;i<h;i++){
-        for(int j = 0;j<w;j++){
-            if(j >= w/2){
-                imga[i][j] = 255-img[i][j];
-            }
-            else{
-                imga[i][j]=img[i][j];
-            }
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w);
+  int imga[MAX_H][MAX_W];
+  for(int i = 0;i<h;i++){
+    for(int j = 0;j<w;j++){
+        if(j >= w/2){
+            imga[i][j] = 255-img[i][j];
+        }
+        else{
+            imga[i][j]=img[i][j];
         }
     }
+  }
     writeImage("taskB.pgm",imga,h,w);
 }
 void box(std::string input){
@@ -75,41 +75,40 @@ void frame(std::string input){
   writeImage("taskD.pgm",imga,h,w);
 }
 void scale(std::string filename){
-    int h, w;
-    int image[MAX_H][MAX_W];
-    int outimage[MAX_H][MAX_W];
-    readImage(filename, image, h, w);
-
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < w; j++){
-            outimage[i*2][j*2] = image[i][j];
-            outimage[i*2][j*2+1] = image[i][j];
-            outimage[i*2+1][j*2] = image[i][j];
-            outimage[i*2 + 1][j*2+1] = image[i][j];
-        }//end inner for loop
-    }//end for loop
-    h = h * 2;
-    w = w * 2;
-    writeImage("taskE.pgm", outimage, h, w);
+  int h, w;
+  int image[MAX_H][MAX_W];
+  int outimage[MAX_H][MAX_W];
+  readImage(filename, image, h, w);
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+          outimage[i*2][j*2] = image[i][j];
+          outimage[i*2][j*2+1] = image[i][j];
+          outimage[i*2+1][j*2] = image[i][j];
+          outimage[i*2 + 1][j*2+1] = image[i][j];
+     }//end inner for loop
+  }//end for loop
+  h = h * 2;
+  w = w * 2;
+  writeImage("taskE.pgm", outimage, h, w);
 }
 
 void pixelate(std::string input)
 {
-    int img[MAX_H][MAX_W];
-    int h, w;
-    readImage(input, img, h, w);
-    int out[MAX_H][MAX_W];
-    int avg;
-    int sum;
-    for(int i = 0; i < h; i+=2){
-        for(int j = 0; j < w; j+=2){
-            sum = img[i][j] + img[i][j+1] + img[i+1][j] + img[i+1][j+1];
-            avg = sum/4;
-            out[i][j] = avg;
-            out[i][j+1] = avg;
-            out[i+1][j] = avg;
-            out[i+1][j+1] = avg;
-        }
-    }
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w);
+  int out[MAX_H][MAX_W];
+  int avg;
+  int sum;
+  for(int i = 0; i < h; i+=2){
+     for(int j = 0; j < w; j+=2){
+          sum = img[i][j] + img[i][j+1] + img[i+1][j] + img[i+1][j+1];
+          avg = sum/4;
+          out[i][j] = avg;
+          out[i][j+1] = avg;
+          out[i+1][j] = avg;
+          out[i+1][j+1] = avg;
+     }
+  }
     writeImage("taskF.pgm", out, h, w);
 }
